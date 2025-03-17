1f83d04459b572d293960e145de9ac3aa3d03ee0
jamesrochabrun <jamesrochabrun@gmail.com>
2025-03-17 16:06:57 -0700

    Adding Claude code as MCP

--- ClaudeTextEditor/ClaudeTextEditorApp.swift
+++ ClaudeTextEditor/ClaudeTextEditorApp.swift
@@ -6,9 +6,26 @@
 //
 
 import SwiftUI
+import MCPSwiftWrapper
 
 @main
 struct ClaudeTextEditorApp: App {
+   
+   
+   init() {
+     let service = AnthropicServiceFactory.service(apiKey: "", betaHeaders: nil, debugEnabled: true)
+
+     // Uncomment this and comment the above for OpenAI Demo
+
+     //      let openAIService = OpenAIServiceFactory.service(apiKey: "", debugEnabled: true)
+     //
+     //      let openAIChatNonStreamManager = OpenAIChatNonStreamManager(service: openAIService)
+     //
+     //      _chatManager = State(initialValue: openAIChatNonStreamManager)
+   }
+
+   private let claudeMCPclient = ClaudeCodeMCP()
+   
    var body: some Scene {
       WindowGroup {
          ContentView()
@@ -17,6 +34,9 @@ struct ClaudeTextEditorApp: App {
                // Optional appearance customization for macOS app
                if let windowScene = NSApplication.shared.windows.first {
                   windowScene.title = "Claude Text Editor"
+                  Task {
+                     await print(try! claudeMCPclient.getClientAsync()?.anthropicTools())
+                  }
                }
             }
       }
